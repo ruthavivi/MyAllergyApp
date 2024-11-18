fae9d27f059e34df3b9d72e673ee3f0c325318ca
ruthavivi <lagami1235@gmail.com>
2024-11-18 19:52:19 +0200

    scan change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,14 @@ export default function RootLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="LoginScreen" />
       <Stack.Screen name="RegisterScreen" />
-      <Stack.Screen name="ProfileScreen" />
+      <Stack.Screen name="CartScreen" />
+      <Stack.Screen name="ChatScreen" />
+      <Stack.Screen name="SettingsScreen" />
+      <Stack.Screen name="EmergencyScreen" />
+      <Stack.Screen name="RestaurantScreen" />
+      <Stack.Screen name="ScanScreen" />
+      <Stack.Screen name="StoreScreen" />
+      <Stack.Screen name="TabNavigator" />
     </Stack>
   );
 }
